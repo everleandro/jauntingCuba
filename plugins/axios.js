3831be812ce506941858c4f5e5d3349df98d0fa2
Leandro <ever.leandros91@gmail.com>
2019-12-10 14:08:53 -0500

    fix: change axios default url

--- plugins/axios.js
+++ plugins/axios.js
@@ -10,7 +10,10 @@
 // export default axios.create(options)
 export default function({ $axios, redirect }) {
   $axios.onRequest(() => {})
-
+  if (process.server) {
+    $axios.options.baseURL = `http://${process.env.HOST || '0.0.0.0'}:${process
+      .env.PORT || 8000}`
+  }
   $axios.onError((error) => {
     const code = parseInt(error.response && error.response.status)
     if (code === 400) {
