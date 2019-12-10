269225d0bcf06d5fcac6f082f072baad5dcf0163
Leandro <ever.leandros91@gmail.com>
2019-12-10 15:19:04 -0500

    fix: change axios default url

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,21 +1,17 @@
-// import * as axios from 'axios'
-//
-// const options = {}
-// // The server-side needs a full url to work
-// if (process.server) {
-//   options.baseURL = `http://${process.env.HOST || '0.0.0.0'}:${process.env
-//     .PORT || 8000}`
-// }
-//
-// export default axios.create(options)
-export default function({ $axios, redirect }) {
-  $axios.onRequest(() => {
-    console.log($axios.defaults.baseURL)
-  })
-  $axios.onError((error) => {
-    const code = parseInt(error.response && error.response.status)
-    if (code === 400) {
-      redirect('/400')
-    }
-  })
+const options = {}
+// The server-side needs a full url to work
+if (process.server) {
+  options.baseURL = `http://${process.env.HOST || 'localhost'}:${process.env
+    .PORT || 3000}`
+}
+
+export default function({ $axios }) {
+  $axios.interceptors.request.use(
+    (config) => {
+      config.baseURL = `http://${process.env.HOST || 'localhost'}:${process.env
+        .PORT || 3000}`
+      return config
+    },
+    (error) => Promise.reject(error)
+  )
 }
